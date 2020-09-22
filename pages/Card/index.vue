916a5c5dfb3cfbf74351875bb7e634e844d14e63
jupiter0913 <jupiter0913@protonmail.com>
2020-09-22 16:27:28 +0200

    Update content page with draggable component.

--- pages/Card/index.vue
+++ pages/Card/index.vue
@@ -3,7 +3,23 @@
     <Header />
     <div class="container">
       <div class="row">
-        This is a Card Page.
+        <draggable v-model="ContentJsonData" style="display: flex; justify-content: flex-start; flex-wrap: wrap">
+          <div
+            class="draggableCardContent"
+            v-for="(element, index) in ContentJsonData"
+            :key="index"
+          >
+            <div class="row pl-2 pr-2 w-100">
+              <div class="col-md-3 pr-2 pl-2">
+                <img class="avatar" :src="require(`~/assets/images/${element.avatar}`)" alt />
+              </div>
+              <div class="col-md-9 pr-2 pl-2">
+                <div>{{element.title}}</div>
+                <div>{{element.description}}</div>
+              </div>
+            </div>
+          </div>
+        </draggable>
       </div>
     </div>
   </div>
@@ -12,13 +28,18 @@
 <script>
 import Vue from "vue";
 import Header from "~/components/Header.vue";
+import draggable from "vuedraggable";
+import ContentJsonData from "@/assets/json/ContentJsonData.json";
 export default {
   components: {
     Header,
+    draggable,
   },
   data: () => ({
+    ContentJsonData: [],
   }),
   mounted() {
+    this.ContentJsonData = ContentJsonData;
   },
   methods: {
   },
