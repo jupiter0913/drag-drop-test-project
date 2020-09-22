f0747864fe3f2c0aafd43bcc4db7cfefd345be74
jupiter0913 <jupiter0913@protonmail.com>
2020-09-22 06:41:42 +0200

    Initial files and folders.

---
+++ components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header class="header" id="header">
+    <div class="top_container">
+      <div class="row align-items-center">
+        <div class="col headerBlockB">
+          <div class="mainNav">
+            <button class="mobileNavBtn">
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
+            <div class="overLay"></div>
+            <nav class="navigation">
+              <ul>
+                <li @click="closeMenu">
+                  <nuxt-link :to="{ path: '/' }">Home</nuxt-link>
+                </li>                
+              </ul>
+            </nav>
+          </div>
+        </div>        
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  mounted() {
+    $(".mobileNavBtn, .overLay").click(function() {
+      if ($(this).parents("body").hasClass("openNav")) {
+        $(this).parents("body").removeClass("openNav");
+      } else {
+        $(this).parents("body").addClass("openNav");
+      }
+    });
+  },
+  methods: {
+    closeMenu() {
+      $(".mobileNavBtn, .overLay").parents("body").removeClass("openNav");
+    }
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrapper">
+    <Header />
+    <div class="container">
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Header from "~/components/Header.vue";
+export default {
+  components: {
+    Header
+  },
+  data: () => ({}),
+};
+</script>
